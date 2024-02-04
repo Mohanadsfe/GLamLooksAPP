af74551868ac40924ce447504153d069c8624449
Mohanad Sfe <92846018+Mohanadsfe@users.noreply.github.com>
2024-02-04 20:11:59 +0200

    Delete app directory

--- app/src/main/java/com/example/glamlooksapp/callback/AuthCallBack.java
+++
@@ -1,9 +0,0 @@
-package com.example.glamlooksapp.callback;
-
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-
-public interface AuthCallBack {
-    void onLoginComplete(Task<AuthResult> task);
-    void onCreateAccountComplete(boolean status , String err);
-}
--- app/src/main/java/com/example/glamlooksapp/callback/UserFetchCallback.java
+++
@@ -1,8 +0,0 @@
-package com.example.glamlooksapp.callback;
-
-import com.example.glamlooksapp.utils.Customer;
-
-// Define a callback interface for user fetch
-public interface UserFetchCallback {
-    void onUserFetchDataComplete(Customer customer);
-}
