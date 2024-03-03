21c8ecd6fe35fce52b1593bd4e87977a9ee8642b
mohanadsafi <mohanadsfe@gmail.com>
2024-03-03 20:40:20 +0200

    update_menuLines

--- app/src/main/java/com/example/glamlooksapp/home/CustomerActivity.java
+++ app/src/main/java/com/example/glamlooksapp/home/CustomerActivity.java
@@ -1,5 +1,9 @@
 package com.example.glamlooksapp.home;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import android.annotation.SuppressLint;
 import android.content.Intent;
@@ -7,8 +11,7 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.Toast;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
+
 import com.example.glamlooksapp.R;
 import com.example.glamlooksapp.auth.LoginActivity;
 import com.example.glamlooksapp.databinding.ActivityCustomerBinding;
@@ -18,29 +21,21 @@ import com.example.glamlooksapp.fragments.user.ProductsFragment;
 import com.example.glamlooksapp.fragments.user.ProfileFragment;
 import com.example.glamlooksapp.fragments.user.AppointmentsListFragment;
 import com.example.glamlooksapp.utils.Database;
-import androidx.fragment.app.FragmentTransaction;
-
 
 public class CustomerActivity extends AppCompatActivity {
     private Database database;
-    private ActivityCustomerBinding customer_Binding;
-
-    public CustomerActivity (){
-
-    }
+    private ActivityCustomerBinding customerBinding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_customer);
+        customerBinding = ActivityCustomerBinding.inflate(getLayoutInflater());
+        setContentView(customerBinding.getRoot());
 
         database = new Database();
-        customer_Binding = ActivityCustomerBinding.inflate(getLayoutInflater());
-        setContentView(customer_Binding.getRoot());
-        replaceFragment(new HomeFragment(CustomerActivity.this));
+        replaceFragment(new HomeFragment());
 
-
-        customer_Binding.bottomNavigationC.setOnItemSelectedListener(item->
+        customerBinding.bottomNavigationC.setOnItemSelectedListener(item->
         {
             switch (item.getItemId()){
                 case R.id.navigation_homeC:
@@ -70,41 +65,34 @@ public class CustomerActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.nav_menu, menu);
         return true;
     }
+
     @SuppressLint("NonConstantResourceId")
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
-
             case R.id.times:
-                Toast.makeText(this,"Times",Toast.LENGTH_SHORT).show();
-                replaceFragment(new AppointmentsListFragment(CustomerActivity.this));
+                Toast.makeText(this, "Times", Toast.LENGTH_SHORT).show();
+                replaceFragment(new AppointmentsListFragment());
                 return true;
-
             case R.id.settings:
-                Toast.makeText(this,"Settings",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Settings", Toast.LENGTH_SHORT).show();
                 return true;
-
             case R.id.exit:
-                Toast.makeText(this,"Exit",Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, "Exit", Toast.LENGTH_SHORT).show();
                 Intent intent = new Intent(this, LoginActivity.class);
                 startActivity(intent);
                 database.logout();
                 finish();
                 return true;
-
             default:
                 return super.onOptionsItemSelected(item);
         }
     }
 
-    private void replaceFragment(Fragment fragment){
-
-        FragmentManager fragmentManger = getSupportFragmentManager();
-        FragmentTransaction fragmentTransaction = fragmentManger.beginTransaction();
-        fragmentTransaction.replace(R.id.fragmentContainerC,fragment);
+    private void replaceFragment(Fragment fragment) {
+        FragmentManager fragmentManager = getSupportFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.replace(R.id.fragmentContainerC, fragment);
         fragmentTransaction.commit();
     }
-
-
-
 }
